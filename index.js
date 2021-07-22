913069b24634bdd6693d89f3c2c39d29a3bd2c61
Allison Homem <mrs.allisonhomem@outlook.com>
2021-07-22 08:49:52 -0500

    finished task 1

--- index.js
+++ index.js
@@ -42,8 +42,31 @@ class Airplane {
 */
 
 class Person {
-  
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+
+  eat(someFood){
+    if(this.stomach.length < 10){
+      this.stomach.push(someFood);
+    }
+  }
+
+  poop(){
+    this.stomach = [];
+  }
+
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
 }
+
+let allison = new Person(`Allison`, 29);
+allison.eat(`avocado toast`);
+allison.eat(`capuccino`);
+console.log(allison);
 
 /*
   TASK 2
